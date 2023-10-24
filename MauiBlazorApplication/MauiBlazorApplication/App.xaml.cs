56e541a57d88d226e76a229e5356cfdafbe8fa9b
Sxulles <seres1322@gmail.com>
2023-10-24 15:58:59 +0200

    Initial Commit

---
+++ MauiBlazorApplication/MauiBlazorApplication/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiBlazorApplication
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ MauiBlazorApplication/MauiBlazorApplication/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiBlazorApplication
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
